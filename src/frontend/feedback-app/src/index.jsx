2da3e59728660f0dab3e76421e8b081c85caa4ae
Xiaohw1999 <wuxiaohui990723@gmail.com>
2024-07-17 20:41:19 +0200

    update parcel to vite

--- src/frontend/feedback-app/src/index.jsx
+++ src/frontend/feedback-app/src/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOMClient from "react-dom/client";
 import { Home } from "./screens/Home";
 
-const app = document.getElementById("app");
+const app = document.getElementById("root");
 const root = ReactDOMClient.createRoot(app);
 root.render(<Home />);
+
